858a9b2fe088bef4ea29d3ba162b0e95ff96ca74
Ayrton Vergara Mondaca <ayrtonvergara50@gmail.com>
2021-07-01 12:20:55 -0600

    terminado

--- js/index.js
+++ js/index.js
@@ -1,69 +1,83 @@
 
-  const servicios = [];
-  const eliminar = function(){
-
-    cargarTabla();
- Swal.fire({
-    position: 'top-center',
-    icon: 'error',
-    title: 'Producto eliminado',
-    showConfirmButton: false,
-    timer: 1500
-  })
-
-    let nro = this.nro;
-    servicios.splice(nro,1);
-    cargarTabla();
-
-  };
-
-  const cargarTabla = ()=>{
-    
-    let tbody = document.querySelector("#tbody-servicio");
-    tbody.innerHTML = "";
-    
-    for(let i=0; i < servicios.length; ++ i){
-       let p = servicios[i];
-       
-       let tr = document.createElement("tr");
+    const servicios = [];
+    const eliminar = function(){
+  
+      cargarTabla();
+   Swal.fire({
+     
+      icon: 'error',
+      title: 'Producto eliminado',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  
+      let nro = this.nro;
+      servicios.splice(nro,1);
+      cargarTabla();
+  
+    };
+  
+    const cargarTabla = ()=>{
       
-       
-       let tdNombre = document.createElement("td");
+      let tbody = document.querySelector("#tbody-servicio");
+      tbody.innerHTML = "";
+      
+      for(let i=0; i < servicios.length; ++ i){
+         let p = servicios[i];
+         
+         let tr = document.createElement("tr");
+        
+         
+         let tdNombre = document.createElement("td");
+  
+         let tdcantidad = document.createElement("td");
+         
+         tdNombre.innerText = p.nombre;
+         tdcantidad.innerText = i + 1;
+  
+          let boton = document.createElement("button");
+          boton.nro = i;
+          boton.addEventListener("click", eliminar);
+          boton.innerText ="eliminar";
+          boton.classList.add("btn","btn-danger");
+  
+          tdNombre.appendChild(boton);
+          tr.appendChild(tdcantidad);
+          tr.appendChild(tdNombre);
+          tbody.appendChild(tr);
+         
+      }
+  };
+   
+   document.querySelector("#servicio-form").addEventListener('submit', (e)=>{
+   e.preventDefault();
+   let nombre = document.querySelector("#nombre-select").value;
+   let esValido = true;
 
-       let tdcantidad = document.createElement("td");
-       
-       tdNombre.innerText = p.nombre;
-       tdcantidad.innerText = i + 1;
+   
+   
+   if(nombre.trim() == ""){
 
-        let boton = document.createElement("button");
-        boton.nro = i;
-        boton.addEventListener("click", eliminar);
-        boton.innerText ="eliminar";
-        boton.classList.add("btn","btn-danger");
-
-        tdNombre.appendChild(boton);
-        tr.appendChild(tdcantidad);
-        tr.appendChild(tdNombre);
-        tbody.appendChild(tr);
-       
-    }
-};
- 
- document.querySelector("#servicio-form").addEventListener('submit', (e)=>{
- e.preventDefault();
- let nombre = document.querySelector("#nombre-select").value;
- let servicio ={};
- 
-servicio.nombre = nombre;
-
-servicios.push(servicio);
- 
- cargarTabla();
- Swal.fire({
-    position: 'top-center',
-    icon: 'success',
-    title: 'Producto Agregado',
-    showConfirmButton: false,
-    timer: 1500
-  })
- });
+    document.querySelector("#nombre-txt").classList.add("is-invalid");
+    esValido = false;
+   }
+   
+   
+  if(esValido){
+  let servicio ={};
+   
+  servicio.nombre = nombre;
+  
+  servicios.push(servicio);
+   
+   cargarTabla();
+   Swal.fire({
+      
+      icon: 'success',
+      title: 'Producto Agregado',
+      showConfirmButton: false,
+      timer: 1500
+   
+    })
+  } 
+   });
